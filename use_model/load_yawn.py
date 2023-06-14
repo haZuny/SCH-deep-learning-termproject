0caab19bee7f6e4ee7559b2a3b662b6c7bc35f18
haZuny <gkwns5791@naver.com>
2023-06-15 02:00:03 +0900

    폴더 정리

--- use_model/load_yawn.py
+++ use_model/load_yawn.py
@@ -3,6 +3,7 @@ import numpy as np
 import tensorflow as tf
 from tensorflow.keras import layers, models
 
+# 이미지 파일로 예측
 def predict_image(file_name):
     
     imageSize = 150
@@ -20,20 +21,19 @@ def predict_image(file_name):
 
 
 # 이미지 불러오기
-path = './yawn_test/12.png'
-
+#path = './yawn_test/12.png'
 #print(predict_image(path))
 
 
 imageSize = 150
 model = tf.keras.models.load_model('./yawn.h5')
-
+# 넘파이 배열로 예측
 def predict_frame(frame):
     
     img = cv2.resize(frame, (imageSize,imageSize))/255
     x = np.array([img])
     predict = model.predict(x)
 
-    if predict[0][0] >= 0.75:
+    if predict[0][0] >= 0.7:
         return 1
     return 0
--- video_test/video_classify.py
+++ video_test/video_classify.py
@@ -1,5 +1,7 @@
-import cv2
+import sys
+sys.path.append('./../use_model')
 import load_yawn
+import cv2
 import time
 
 capture = cv2.VideoCapture('./test-video3.mp4')
@@ -15,11 +17,13 @@ while capture.isOpened():
     if run and current_time > 1./FPS:
         prev_time = time.time()
     
+        # 하품 여부 탐색
         if load_yawn.predict_frame(frame) == 1:
             isYawnText = 'yawn'
         else:
             isYawnText = "no-yawn"
         
+        # 하품 라벨링
         cv2.putText(img=frame,text=isYawnText, org=(100,100), fontFace=cv2.FONT_HERSHEY_PLAIN,
                     fontScale = 7, color=(0,0,255), thickness=10, bottomLeftOrigin=False)
         
